67deff8d2116f524b3b16e2eff51e90ef349e368
unknown <youyu00022@gmail.com>
2024-04-14 19:20:17 -0500

    updated the inquirer version and added functions

--- index.js
+++ index.js
@@ -1,2 +1,78 @@
 console.log('hello');
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+async function generateLogo() {
+    // Prompt user for input
+    const questions = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter the text for the logo:',
+            validate: function (input) {
+                const words = input.split(/\s+/);
+                if (words.length > 3) {
+                    return 'Please limit the description to 3 words.';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color should the text be? (e.g., #ff0000 or color name):',
+        },
+        {
+            type: 'list',
+            message: 'What shape should the logo be?',
+            name: 'shape',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color should the shape be? (e.g., #ff0000 or color name)'
+        },
+
+    ]);
+
+    const answers = await inquirer.prompt(questions);
+    return answers;
+
+}
+
+function createLogo(answers) {
+    const { text, textColor, shape, shapeColor } = answers;
+    const newLogo = {
+        'circle': `<circle cx="50" cy="50" r="40" fill="${shapeColor}" />`,
+        'triangle': `<polygon points="50,15 20,80 80,80" fill="${shapeColor}" />`,
+        'square': `<rect width="70" height="70" x="15" y="15" fill="${shapeColor}" />`
+    };
+    return`
+    <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+        ${newLogo[shape]}
+        <text x="50%" y="90%" style="dominant-baseline: middle; text-anchor: middle; fill: ${textColor}; font-family: Arial" font-size="10">${text}</text>
+    </svg>`;
+    
+}
+
+// Steps the program begins from start to finish
+async function init(){
+    try {
+        const answers = await generateLogo();
+        const logoContent = createLogo(answers);
+        fs.writeFileSync('logo.svg', logoContent);
+        console.log('Logo is generated and saved as logo.svg')
+    } catch (error) {
+        console.error('failed to generate logo: ', error);
+    }
+
+}
+
+
+
+
+// Initializes the program
+init();
